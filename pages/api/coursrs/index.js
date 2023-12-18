a7a08c950abfae9110283375348086d6eeb8b579
alirezashn79 <alireza.sharifinasabb@yahoo.com>
2023-12-19 00:59:35 +0330

    add post api to create courses

---
+++ pages/api/coursrs/index.js
@@ -0,0 +1,26 @@
+const { default: coursesModel } = require("@/models/course");
+const { default: connectToDB } = require("@/utils/db");
+
+const handler = async (req, res) => {
+  await connectToDB();
+  if (req.method === "POST") {
+    try {
+      const { title, price, teacher } = req.body;
+
+      if (!title.trim() || !teacher.trim() || !price.trim()) {
+        return res.status(422).json({ message: "invalid data...!" });
+      }
+
+      const course = await coursesModel.create({ title, price, teacher });
+
+      return res
+        .status(201)
+        .json({ message: "Course created successfully :))", data: course });
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ message: "internal server error...!", data: err });
+    }
+  }
+};
+export default handler;
---
+++ pages/api/index.js
@@ -0,0 +1,4 @@
+const handler = async (req, res) => {
+  return res.json({ message: "Welcome to next-cms api route" });
+};
+export default handler;
