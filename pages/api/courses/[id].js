2327beba8a6a7ab0c8dd2adb8e467f0bf5d78bc9
alirezashn79 <alireza.sharifinasabb@yahoo.com>
2023-12-19 20:58:39 +0330

    develop update course api

--- pages/api/courses/[id].js
+++ pages/api/courses/[id].js
@@ -24,6 +24,27 @@ const handler = async (req, res) => {
         .status(500)
         .json({ message: "internal server error...!", data: err });
     }
+  } else if (req.method === "PUT") {
+    // get dynamic id
+    const { id } = req.query;
+    // get request body
+    const reqBody = req.body;
+    // check course
+    if (!isValidObjectId(id)) {
+      return res.status(422).json({ message: "course is unavailable...!" });
+    }
+
+    try {
+      const course = await coursesModel.findByIdAndUpdate(id, reqBody);
+      return res.json({
+        message: "course updated successfully :))",
+        data: course,
+      });
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ message: "internal server error...!", data: err });
+    }
   }
 };
 
